90244973a7ba4d743961fbcc9280b41a5e6e600d
Mohsen Afshari <afshari.mvc@gmail.com>
2021-02-17 13:37:46 +0330

    Add IDS Interface

--- main.js
+++ main.js
@@ -1,19 +1,53 @@
 const { app, BrowserWindow } = require('electron')
 const ejse = require('ejs-electron')
+const contextMenu = require('electron-context-menu');
+const SerialPort = require('serialport');
+var nodeConsole = require('console');
 
-function createWindow () {
+
+contextMenu({
+	prepend: (defaultActions, params, browserWindow) => [ {
+			label: 'Rainbow',
+			// Only show it when right-clicking images
+			visible: params.mediaType === 'image'
+		}, {
+			label: 'Search Google for “{selection}”',
+			// Only show it when right-clicking text
+			visible: params.selectionText.trim().length > 0,
+			click: () => {
+				shell.openExternal(`https://google.com/search?q=${encodeURIComponent(params.selectionText)}`);
+			}
+		}
+	]
+});
+
+
+var myConsole = new nodeConsole.Console(process.stdout, process.stderr);
+console.log("TEST");
+console.log(SerialPort.list());
+
+SerialPort.list().then(function(ports){
+  ports.forEach(function(port){
+    console.log("Port: ", port);
+  })
+});
+
+function createWindow (ports) {
   const win = new BrowserWindow({
-    width: 800,
-    height: 600,
     webPreferences: {
+			enableRemoteModule: true,
       nodeIntegration: true
     }
   })
-
-  win.loadFile('front_end/blank.ejs')
+	win.maximize()
+  win.loadFile('front_end/ids.ejs', {query: {"ports": JSON.stringify(ports)}})
 }
 
-app.whenReady().then(createWindow)
+app.whenReady().then(function() {
+	SerialPort.list().then(function(ports){
+		createWindow(ports)
+	});
+});
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
@@ -23,6 +57,8 @@ app.on('window-all-closed', () => {
 
 app.on('activate', () => {
   if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow()
+		SerialPort.list().then(function(ports){
+			createWindow(ports)
+		});
   }
 })
