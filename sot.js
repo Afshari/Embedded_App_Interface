f16168eb64a68b94acd594808beab1f8b0c497c9
Mohsen Afshari <afshari.mvc@gmail.com>
2021-05-06 13:42:59 +0430

    Add a new data format for communicating between SOT Client & Server

--- sot.js
+++ sot.js
@@ -8,17 +8,12 @@ let mainWindow;
 
 module.exports = {
     init,
-    readDataFile,
+    // readDataFile,
     connect,
     isActive,
     deactivate
 }
 
-// let listOfData
-// var dataCounter = 0
-// let prior
-// let listOfGroundTruth
-// var isAc
 var _isActive = false;
 var _isConnected = false;
 
@@ -29,14 +24,6 @@ function init(win) {
     const timeoutObj = setTimeout(function () {
         
         connect()
-
-        // listOfData = readDataFile('/Users/mohsen/Documents/state_estimate_ws/MOT/stonesoup/PDA/sot_data.txt')
-        // prior = getPrior('/Users/mohsen/Documents/state_estimate_ws/MOT/stonesoup/PDA/sot_data.txt')
-        
-        // listOfGroundTruth = readGroundTruthFile('/Users/mohsen/Documents/state_estimate_ws/MOT/stonesoup/PDA/ground_truth.txt')
-        // loopThroughSplittedText(listOfData, listOfGroundTruth)
-
-        // sendPrior(prior)
         
         _isActive = true;
         clearTimeout(timeoutObj)
@@ -56,44 +43,13 @@ function deactivate() {
     if(_isConnected == true) {
 
         _isConnected = false;
-        client.write('EOF');
+        client.write('29:-');
         client.end();
         client.destroy();
     }
 }
  
   
-function loopThroughSplittedText(values, ground_truth) {
-    // for (var i = 0; i < values.length; i++) {
-    for (var i = 0; i < 2; i++) {
-        // for each iteration console.log a word
-        // and make a pause after it
-        (function (i) {
-            setTimeout(function () {
-
-                mainWindow.webContents.send('sot:draw', { 
-                    type: 'circle', 
-                    x: values[i]['x'], 
-                    y: values[i]['y'], 
-                    r: 5,
-                    covar: values[i]['covar'],
-                    measurements: values[i]['measurements'],
-                    truthX: ground_truth[i]['x'],
-                    truthY: ground_truth[i]['y']
-                })
-
-            }, 1000 * i);
-        })(i);
-    };
-}
-
-function getPrior(path) {
-
-    const data = fs.readFileSync(path, 'utf-8')
-    arr = data.split('\r\n')
-    return [ arr[0].replace('x: ', ''), arr[1].replace('P: ', '') ]
-}
-
 
 function readDataFile(path) {
 
@@ -128,32 +84,6 @@ function readDataFile(path) {
 }
 
 
-function readGroundTruthFile(path) {
-
-    var listOfData = []
-
-    const data = fs.readFileSync(path, 'utf-8')
-   
-
-    arr = data.split('\r\n')
-    for(var i = 0; i < arr.length; i++) {
-
-        if(arr[i].length > 0) {
-
-            var state = arr[i].split(':')[2]
-            
-            state = state.split(',')
-            listOfData.push({ 
-                x: parseFloat(state[0]),
-                y: parseFloat(state[2])
-            })
-        }        
-
-    }
-    return listOfData;
-}
-
-
 var client = new net.Socket();
 
 function connect() {
@@ -164,19 +94,24 @@ function connect() {
 }
 
 
-function sendPrior(prior) {
-    // client.write(prior[0] + '|' + prior[1] + '|40:')
-    // client.write('prior: ' + prior)
-}
 
 ipcMain.on('tracking:send:prior', (event, data) => {
 
-    client.write('prior: ' + data)
+    // data = data.replace(',measure', '')
+    data.splice(2, 1)
+    // client.write('20:2, 1:' + data)
+    client.write(`27:10|20:4, 1:${data[0]},0.1,${data[1]},0.1|21:4, 4:0.5,0,0,0,  0,0.1,0,0,  0,0,0.5,0,  0,0,0,0.1`)
 })
 
 ipcMain.on('tracking:send:measurements', (event, data) => {
 
-    client.write('measurements: ' + data)
+    strData = "22"
+    data.forEach(function(item, index, array) {
+        strData += `:23:2, 1:${item[0]},${item[1]}|89:`
+    })
+
+    // console.log(strData)
+    client.write(strData)
 })
 
 
@@ -187,61 +122,16 @@ function sendMeasurement(measurement, dt) {
 
 client.on('data', function(data) {
 
-    data = data.toString().split(',')
+    data = data.toString().replace('\\n', '').split('|')[0]
+    data = data.replaceAll('[', '').replaceAll(']', '').split('\n')
+    // console.log( parseInt( parseFloat( data[0] ) ) )
+    // console.log( parseInt( parseFloat( data[2] ) ) )
     // console.log()
     // console.log(parseInt(data[0]), parseInt(data[1]))
-    mainWindow.webContents.send('sot:draw', parseInt(data[0]), parseInt(data[1]));
+    mainWindow.webContents.send('sot:draw', parseInt( parseFloat( data[0] ) ), parseInt( parseFloat( data[2] ) ));
 })
 
 
-// client.on('data', function(data) {
-	
-//     if(data == '50') {
-//         console.log("State 1 Finished")
-        
-//         sendMeasurement(listOfData[dataCounter]['measurements'], listOfData[dataCounter]['dt'])
-
-//     } else {
-
-//         data = data.toString()
-//         data = data.split('|')
-//         var x = data[0].replaceAll('\n', ' ')
-//         x = x.split(' ').filter( element => element != '')
-//         var covar = data[1].replaceAll('\n', ' ')
-//         covar = covar.split(' ').filter( element => element != '')
-//         covar = covar.join(', ')
-        
-//         mainWindow.webContents.send('sot:draw', { 
-//             type: 'circle', 
-//             x: x[0],
-//             y: x[2],
-//             r: 5,
-//             covar: covar,
-//             measurements: listOfData[dataCounter]['measurements'], 
-//             truthX: listOfGroundTruth[dataCounter]['x'],
-//             truthY: listOfGroundTruth[dataCounter]['y']
-//         })
-
-        
-//         dataCounter += 1
-//         if(dataCounter < listOfData.length) {
-
-//             const timeoutObj = setTimeout(function () {
-        
-//                 sendMeasurement(listOfData[dataCounter]['measurements'], listOfData[dataCounter]['dt']) 
-//                 clearTimeout(timeoutObj)
-        
-//             }, 1000)
-
-//             // sendMeasurement(listOfData[dataCounter]['measurements'], listOfData[dataCounter]['dt'])
-
-//         } else {
-//             console.log("Operation Finished")
-//             client.destroy(); // kill client after server's response
-//         }
-//     }
-	
-// });
 
 client.on('close', function() {
 	console.log('Connection closed');
